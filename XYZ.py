bf3966485b27de6415d226442dafa7b7de6a2e1f
thepipboy <3476647793@qq.com>
2025-03-23 20:34:46 +0800

    Create XYZ.py

---
+++ XYZ.py
@@ -0,0 +1,21 @@
+import position
+def space(x,y,z,X,Y,Z):
+        
+        this.x = new
+        if (X.position + 1),(Y.posssition),(Z.position)
+        return key.D
+        else (X.position - 1),(Y.posssition),(Z.position)
+        return key.A
+        
+        this.y = new 
+        if (X.position),(Y.position + 1),(Z.position)
+        return key.W
+        else (X.position),(Y.position - 1),(Z.position)
+        return key.S
+        
+        this.z = new 
+        if (X.position),(Y.position),(Z.position + 1)
+        return key.space
+        else (X.position),(Y.position),(Z.position - 1)
+        return key.shift
+        
