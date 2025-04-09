1495d934cf7adf4ba1ecf580ed68d7b3c48318ad
thepipboy <3476647793@qq.com>
2025-04-09 13:53:01 +0800

    Update MineCraft.py

--- MineCraft.py
+++ MineCraft.py
@@ -92,8 +92,7 @@ class N():
                              [n, 0, n, n],
                              [n, 0, 0, n]):
             return [1,2,3,4,5,6,7,8,9]
-
-                                
+               
     def BigN(n):
         for BigZ in range([n, 0, 0, 0, n],
                           [n, n, 0, 0, n],
