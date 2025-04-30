844eaf2dcd78a21f797d7997fd662e134e1aa7da
thepipboy <3476647793@qq.com>
2025-04-30 22:47:44 +0800

    Update time.py

--- time.py
+++ time.py
@@ -11,11 +11,10 @@ class abcdef():
       d = "00001110"
       e = "00001111"
       f = "00010000"
-    def time(a,b,c,d,e,f,x,y,z,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12):
+    def time(a,b,c,d,e,f,x,y,z,t):
         return [
-            a * math.sin(x + t1) + b * math.cos(x - t1) + c * math.sin(y + t2) + d * math.cos(y - t2) + e * math.sin(z + t3) + f * math.cos(z - t3),       #t123
-            a * math.sin(x + t1) + b * math.cos(x - t1) + c * math.sin(y + t2) + d * math.cos(y - t2) + e * math.sin(z + t3) + f * math.cos(z - t3),       #t123
-            a * math.asin(x + t4) + b * math.acos(x - t4) + c * math.asin(y + t5) + d * math.acos(y - t5) + e * math.asin(z + t6) + f * math.acos(z - t6), #t456
-            a * math.sinh(x + t7) + b * math.cosh(x - t7) + c * math.sinh(y + t8) + d * math.cosh(y - t8) + e * math.sinh(z + t9) + f * math.cosh(z - t9), #t789
-            a * math.tan(x + t10) + b * math.atan(x - t10) + c * math.tan(y + t11) + d * math.atan(y - t11) + e * math.tan(z + t12) + f * math.atan(z - t12) #t101112
+            a * math.sin(x + t) + b * math.cos(x - t) + c * math.sin(y + t) + d * math.cos(y - t) + e * math.sin(z + t) + f * math.cos(z - t),       #t123
+            a * math.sin(x + t) + b * math.cos(x - t) + c * math.sin(y + t) + d * math.cos(y - t) + e * math.sin(z + t) + f * math.cos(z - t),       #t456
+            a * math.asin(x + t) + b * math.acos(x - t) + c * math.asin(y + t) + d * math.acos(y - t) + e * math.asin(z + t) + f * math.acos(z - t), #t789
+            a * math.tan(x + t) + b * math.atan(x - t) + c * math.tan(y + t) + d * math.atan(y - t) + e * math.tan(z + t) + f * math.atan(z - t) #t101112
             ]
