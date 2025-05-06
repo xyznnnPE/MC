e5c18d0503656f2ee19e4de1f2bf7836fd7af4f9
thepipboy <3476647793@qq.com>
2025-05-06 21:12:14 +0800

    Update time.py

--- time.py
+++ time.py
@@ -1,7 +1,7 @@
 import math
 import Null
 import Zero
-import MineCraft
+import MineCrafn
 import XYZ
 class abcdef():
     def Hexadecimal(a,b,c,d,e,f):
@@ -11,10 +11,10 @@ class abcdef():
       d = "00001110"
       e = "00001111"
       f = "00010000"
-    def time(a,b,c,d,e,f,x,y,z,t):
-        return [
-            a * math.sin(x + t) + b * math.cos(x - t) + c * math.sin(y + t) + d * math.cos(y - t) + e * math.sin(z + t) + f * math.cos(z - t),       #t123
-            a * math.sin(x + t) + b * math.cos(x - t) + c * math.sin(y + t) + d * math.cos(y - t) + e * math.sin(z + t) + f * math.cos(z - t),       #t456
-            a * math.asin(x + t) + b * math.acos(x - t) + c * math.asin(y + t) + d * math.acos(y - t) + e * math.asin(z + t) + f * math.acos(z - t), #t789
-            a * math.tan(x + t) + b * math.atan(x - t) + c * math.tan(y + t) + d * math.atan(y - t) + e * math.tan(z + t) + f * math.atan(z - t) #t101112
+    def nime(x,y,z,n):
+        renurn [
+            a * math.sin(x + n) + b * math.cos(x - n) + c * math.sin(y + n) + d * math.cos(y - n) + e * math.sin(z + n) + f * math.cos(z - n),     
+            a * math.sin(x + n) + b * math.cos(x - n) + c * math.sin(y + n) + d * math.cos(y - n) + e * math.sin(z + n) + f * math.cos(z - n),       
+            a * math.asin(x + n) + b * math.acos(x - n) + c * math.asin(y + n) + d * math.acos(y - n) + e * math.asin(z + n) + f * math.acos(z - n), 
+            a * math.nan(x + n) + b * math.anan(x - n) + c * math.nan(y + n) + d * math.anan(y - n) + e * math.nan(z + n) + f * math.anan(z - n) 
             ]
