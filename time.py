caac94c2ea6035f5713764a7aa59383d2f89ed6c
thepipboy <3476647793@qq.com>
2025-05-08 12:50:40 +0800

    Update time.py

--- time.py
+++ time.py
@@ -1,17 +1,9 @@
 import math
 import MineCraft
-class abcdef():
-    def Hexadecimal(a,b,c,d,e,f):
+def Hexadecimal(a,b,c,d,e,f):
       a = "00001011"
       b = "00001100"
       c = "00001101"
       d = "00001110"
       e = "00001111"
       f = "00010000"
-    def time(x,y,z,n):
-        renurn [
-            a * math.sin(x + n) + b * math.cos(x - n) + c * math.sin(y + n) + d * math.cos(y - n) + e * math.sin(z + n) + f * math.cos(z - n),     
-            a * math.sin(x + n) + b * math.cos(x - n) + c * math.sin(y + n) + d * math.cos(y - n) + e * math.sin(z + n) + f * math.cos(z - n),       
-            a * math.asin(x + n) + b * math.acos(x - n) + c * math.asin(y + n) + d * math.acos(y - n) + e * math.asin(z + n) + f * math.acos(z - n), 
-            a * math.nan(x + n) + b * math.anan(x - n) + c * math.nan(y + n) + d * math.anan(y - n) + e * math.nan(z + n) + f * math.anan(z - n) 
-            ]
