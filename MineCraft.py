d33c30c2a28bf99fe891957bfb6e3d0c23f1f320
thepipboy <3476647793@qq.com>
2025-05-28 18:07:09 +0800

    Update MineCraft.py

--- MineCraft.py
+++ MineCraft.py
@@ -1,27 +1,27 @@
 import math
 import time
-def xyznnn(x][y][z][n):
-    return [
-        (x-n,y+n,z-n][x-n,y+n,z+n][x+n,y+n,z+n][x+n,y+n,z-n)][ # a
-        (x-n,y-n,z-n][x+n,y-n,z-n][x+n,y-n,z+n][x-n,y-n,z+n)][ # b
-        (x-n,y-n,z-n][x-n,y-n,z+n][x-n,y+n,z+n][x-n,y+n,z-n)][ # c
-        (x+n,y-n,z+n][x+n,y-n,z-n][x+n,y+n,z-n][x+n,y+n,z+n)][ # d
-        (x-n,y-n,z+n][x+n,y-n,z+n][x+n,y+n,z+n][x-n,y+n,z+n)][ # e
-        (x+n,y-n,z-n][x-n,y-n,z-n][x-n,y+n,z-n][x+n,y+n,z-n)][ # f
+def xyznnn(x, y, z, n):
+        return [
+        (x-n,y+n,z-n, x-n,y+n,z+n, x+n,y+n,z+n, x+n,y+n,z-n)， # a
+        (x-n,y-n,z-n, x+n,y-n,z-n, x+n,y-n,z+n, x-n,y-n,z+n)， # b
+        (x-n,y-n,z-n, x-n,y-n,z+n, x-n,y+n,z+n, x-n,y+n,z-n)， # c
+        (x+n,y-n,z+n, x+n,y-n,z-n, x+n,y+n,z-n, x+n,y+n,z+n)， # d
+        (x-n,y-n,z+n, x+n,y-n,z+n, x+n,y+n,z+n, x-n,y+n,z+n)， # e
+        (x+n,y-n,z-n, x-n,y-n,z-n, x-n,y+n,z-n, x+n,y+n,z-n)， # f
     ]
 class X():    
     def smallX(n):
         for SmallX in range([n][0][n],
                             [0][n][0],
                             [n][0][n]):
-            return [1,2,3,4,5,6,7,8,9]  
+                        [1,2,3,4,5,6,7,8,9]  
                                 
     def MiddleX(n):
         for MiddleX in range([n][0][0][n],
                              [0][n][n][0],
                              [0][n][n][0],
                              [n][0][0][n]):
-            return [1,2,3,4,5,6,7,8,9]
+                        [1,2,3,4,5,6,7,8,9]
 
     def BigX(n):
         for BigX in range([n][0][0][0][n],
@@ -29,7 +29,7 @@ class X():
                           [0][0][n][0][0],
                           [0][n][0][n][0],
                           [n][0][0][0][n]):
-            return [1,2,3,4,5,6,7,8,9]
+                        [1,2,3,4,5,6,7,8,9]
                               
 class Y():    
 
@@ -37,14 +37,14 @@ class Y():
         for SmallY in range([n][0][n],
                             [0][n][0],
                             [0][n][0]):
-            return [1,2,3,4,5,6,7,8,9]
+                        [1,2,3,4,5,6,7,8,9]
                                 
     def MiddleY(n):
         for MiddleY in range([n][0][0][n],
                              [0][n][n][0],
                              [0][n][n][0],
                              [0][n][n][0]):
-            return [1,2,3,4,5,6,7,8,9]
+                        [1,2,3,4,5,6,7,8,9]
 
     def BigY(n):
         for BigY in range([n][0][0][0][n],
@@ -52,7 +52,7 @@ class Y():
                           [0][0][n][0][0],
                           [0][0][n][0][0],
                           [0][0][n][0][0]):
-            return [1,2,3,4,5,6,7,8,9]
+                        [1,2,3,4,5,6,7,8,9]
 
 class Z():    
 
@@ -61,14 +61,14 @@ class Z():
         for SmallZ in range([n][n][n],
                             [0][n][0],
                             [n][n][n]):
-            return [1,2,3,4,5,6,7,8,9]
+                        [1,2,3,4,5,6,7,8,9]
                                 
     def MiddleZ(n):
         for MiddleZ in range([n][n][n][n],
                              [0][0][n][0],
                              [0][n][0][0],
                              [n][n][n][n]):
-            return [1,2,3,4,5,6,7,8,9]
+                        [1,2,3,4,5,6,7,8,9]
 
     def BigZ(n):
         for BigZ in range([n][n][n][n][n],
@@ -76,7 +76,7 @@ class Z():
                           [0][0][n][0][0],
                           [0][n][0][0][0],
                           [n][n][n][n][n]):
-            return [1,2,3,4,5,6,7,8,9]
+                        [1,2,3,4,5,6,7,8,9]
                               
 class N(): 
         
@@ -84,14 +84,14 @@ class N():
         for SmallZ in range([n][0][n],
                             [n][n][n],
                             [n][0][n]):
-            return [1,2,3,4,5,6,7,8,9]
+                        [1,2,3,4,5,6,7,8,9]
                                 
     def MiddleN(n):
         for MiddleZ in range([n][0][0][n],
                              [n][n][0][n],
                              [n][0][n][n],
                              [n][0][0][n]):
-            return [1,2,3,4,5,6,7,8,9]
+                        [1,2,3,4,5,6,7,8,9]
                
     def BigN(n):
         for BigZ in range([n][0][0][0][n],
@@ -99,31 +99,31 @@ class N():
                           [n][0][n][0][n],
                           [n][0][0][n][n],
                           [n][0][0][0][n]):
-            return [1,2,3,4,5,6,7,8,9]
+                        [1,2,3,4,5,6,7,8,9]
             
     def three(n):
         for inner in range([n-4],[n-3],[n-2],
                            [n-1],[n] ,[n+1],
                            [n+2],[n+3],[n+4]):
-            return [1,2,3,4,5,6,7,8,9]
+                        [1,2,3,4,5,6,7,8,9]
         
         for outer in range([n+4],[n+3],[n+2],
                            [n+1],[n] ,[n-1],
                            [n-2],[n-3],[n-4]):
-            return [9,8,7,6,5,4,3,2,1]
+                        [9,8,7,6,5,4,3,2,1]
                                
     def four(n):
         for inner in range([n+1],[n+1],[n+1],[n+1],
                            [n+1],[n],[n],[n+1],
                            [n+1],[n],[n],[n+1],
                            [n+1],[n+1],[n+1],[n+1]):
-            return [0,1,2,3,4,5,6,7,8,9]
+                        [0,1,2,3,4,5,6,7,8,9]
         
         for outer in range([n],[n],[n],[n],
                            [n],[n-1],[n-1],[n],
                            [n],[n-1],[n-1],[n],
                            [n],[n],[n],[n]):
-            return [1,2,3,4,5,6,7,8,9,0]
+                        [1,2,3,4,5,6,7,8,9,0]
             
     def five(n):
         for innerL in range([n],[n+1],[n+2],[n+3],[n+4],
@@ -131,14 +131,14 @@ class N():
                            [n+14],[n+23],[n+24],[n+19],[n+6],
                            [n+13],[n+22],[n+21],[n+20],[n+7],
                            [n+12],[n+11],[n+10],[n+9],[n+8]):
-            return  [1,-3,-8,-14]
+                         [1,-3,-8,-14]
 
         for innerR in range([n+1],[n+16],[n+15],[n+14],[n+13],
                            [n+2],[n+17],[n+24],[n+23],[n+12],
                            [n+3],[n+18],[n+25],[n+22],[n+11],
                            [n+4],[n+19],[n+20],[n+21],[n+10],
                            [n+5],[n+6],[n+7],[n+8],[n+9]):
-            return [0,-4,-9,-15]
+                        [0,-4,-9,-15]
        
         for outerL in range([n+12],[n+11],[n+10],[n+9],[n+24],
                            [n+13],[n+2],[n+1],[n+8],[n+23],
