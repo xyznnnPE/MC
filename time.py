d300d16ea899b0523f2d9524094abb9525b39069
xyznnn <3476647793@qq.com>
2025-10-07 16:39:03 +0800

    Delete time.py

--- time.py
+++
@@ -1,9 +0,0 @@
-import math
-import MineCraft
-def Hexadecimal(a,b,c,d,e,f):
-      a = "00001011"
-      b = "00001100"
-      c = "00001101"
-      d = "00001110"
-      e = "00001111"
-      f = "00010000"
