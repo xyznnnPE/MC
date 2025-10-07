93ef8e458e2f52323e60c6a8cf5811ab06e197fd
xyznnn <3476647793@qq.com>
2025-10-07 17:34:02 +0800

    Create color.py

---
+++ color.py
@@ -0,0 +1,176 @@
+import math
+def Red():
+     element1=
+     [[1][0][0][0],
+      [0][1][0][0],
+      [0][0][1][0],
+      [0][0][0][1]]
+
+     element2=
+     [[0][1][0][0],
+      [0][0][1][0],
+      [0][0][0][1],
+      [1][0][0][0]]
+
+     element3=
+     [[0][0][0][1],
+      [1][0][0][0],
+      [0][1][0][0],
+      [0][0][1][0]]
+
+     element4 =
+     [[0][0][1][0],
+      [0][0][0][1],
+      [1][0][0][0],
+      [0][1][0][0]]
+
+print(element1,element2,element3,element4)
+def Green():
+    element5 =
+    [[1][0][1][0],
+     [0][1][0][0],
+     [0][0][1][0],
+     [0][0][0][1]]
+
+    element6 =
+    [[1][0][0][0],
+     [0][1][0][1],
+     [0][0][1][0],
+     [0][0][0][1]]
+
+    element7 =
+    [[1][0][0][0],
+     [0][1][0][0],
+     [0][0][1][0],
+     [0][1][0][1]]
+     
+    element8 =
+    [[1][0][0][0],
+     [0][1][0][0],
+     [1][0][1][0],
+     [0][0][0][1]]
+
+print(element5,element6,element7,element8)
+def Blue():
+     element9 =
+    [[0][1][0][1],
+     [1][0][1][0],
+     [0][0][0][1],
+     [0][0][0][0]]
+
+     element10 =
+    [[0][1][0][1],
+     [0][0][1][0],
+     [0][1][0][1],
+     [0][0][0][0]]
+
+     element11 =
+    [[0][1][0][1],
+     [0][0][1][0],
+     [0][0][0][1],
+     [0][0][1][0]]
+ 
+     element12 =
+    [[0][1][0][1],
+     [0][0][1][0],
+     [0][0][0][1],
+     [1][0][0][0]]
+
+print(element9,element10,element11,element12)
+def white():
+    element13 =
+    [[1][0][1][0],
+     [0][0][0][1],
+     [1][0][0][0],
+     [0][1][0][0]]
+
+    element14 =
+    [[0][0][1][0],
+     [0][1][0][1],
+     [1][0][0][0],
+     [0][1][0][0]]
+
+    element15 =
+    [[0][0][1][0],
+     [0][0][0][1],
+     [1][0][1][0],
+     [0][1][0][0]]
+
+    element16 =
+    [[0][0][1][0],
+     [0][0][0][1],
+     [1][0][0][0],
+     [0][1][0][1]]
+
+    element17 =
+    [[0][1][0][0],
+     [1][0][0][0],
+     [0][1][0][0],
+     [1][0][1][0]]
+
+    element18 =
+    [[0][0][0][0],
+     [1][0][1][0],
+     [0][1][0][0],
+     [1][0][1][0]]
+
+    element19 =
+    [[0][0][0][0],
+     [1][0][0][0],
+     [0][1][0][1],
+     [0][0][1][0]]
+
+    element20 =
+    [[0][0][0][1],
+     [1][0][0][0],
+     [0][1][0][0],
+     [0][0][1][0]]
+
+print(element13,element14,element15,element16,element17,element18,element19,element20)
+class Red(CH4):
+def C1H4(C=1,H=4): pass
+def C5H8(C=5,H=8): pass
+def C13H24(C=13,H=24): pass
+    print(C1H4,C5H8,C13,H24)
+class Green(CH22):
+def C2H2(C=2,H=2): pass
+def C4H2(C=4,H=2): pass
+def C6H2(C=6,H=2): pass
+    print(C2H2,C4H2,C6H2)
+class Blue(CH24):
+def C2H4(C=2,H=4): pass
+def C6H8(C=6,H=8): pass
+def C14H16(C=16,H=16): pass
+    print(C2H4,C6H8,C14H12)
+class white(CH4,CH22,CH24):
+def CH4(C1H4,C5H8,C13H24): *yield
+def CH22(C2H2,C4H2,C6H2): *yield
+def CH24(C2H4,C6H8,C14H16): *yield
+class Sphere():
+    self.X = Red,self.Y = Green,slef.Z = Blue,self.T = white
+def SpherePoint(r = Radius):
+    char X = r * sin(theta) * cos(fai)
+    char Y = r * sin(theta) * sin(fai)
+    char Z = r * cos(fai)
+    char T = r * sin(theta)
+    return [X,Y,Z,T]
+def SphereCylinder(X->CylinderXY,Y->CylinderYZ,Z->CylinderXZ,T->CylinderXYZ):
+    X ^ 2 + Y ^ 2 + Z ^ 2 === T ^ 6
+    return "CylinderXY,CylinderYZ,CylinderXZ,CylinderXYZ"
+def SphereRadius(Radius):
+    const SphereRadiusx = sin(y) + cos(z) 
+    const SphereRadiusy = sin(x) + cos(z) 
+    const SphereRadiusz = sin(y) + cos(x)
+    return "SphereRadiusx, SphereRadiusy, SphereRadiusz"
+class impact(element):
+const impact0 = "element1 + element2 + element3 + element4"
+const impact1 = "element5 - element6 - element7 - element8"
+const impact2 = "element9 * element10 * element11 * element12"
+const impact3 = "element13 / element14 / element15 / element16"
+const impact4 = "element17 % element18 % element19 % element20"
+
+const impact5 = "impact0 * impact1" + "impact2 * impact3"
+const impact6 = "impact1 - impact2" + "impact3 - impact4"
+const impact7 = "impact2 / impact3" + "impact4 / impact0"
+const impact8 = "impact3 % impact4" + "impact1 % impact2"
+print(impact0, impact1, impact2, impact3, impact4, impact5, impact6, impact7, impact8)
